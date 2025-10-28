965a79b2b938e137ff6567ae5ef5e7304ba4a882
DarniRajKumar <rajkumardarni@b2btesters.com>
2025-10-28 15:01:22 +0530

    Add high priority cases dashboard with API route

---
+++ app/highpriorityv2/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+export default function HighPriorityV2Page() {
+  return (
+    <iframe
+      src="/highpriorityv2.html"
+      title="High Priority CCMS2 Cases Dashboard"
+      style={{ width: '100%', height: '100vh', border: '0' }}
+    />
+  )
+}
+
+
